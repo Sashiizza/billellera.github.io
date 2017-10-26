7a7cde4536de588c46f69647f58e661923a964dc
Sashiizza <omminosam@gmail.com>
2017-10-26 23:20:15 +0200

    bau

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ app.controller('indexCtrl', function($scope, $http) {
 
     $http.get ('poollogs.json').then (function (res) {
         $scope.lastpayout = res.data.lastpayout * 1000;
+        $scope.nextpayout = moment ($scope.lastpayout).add (1, 'day').valueOf();
         $scope.accounts = [];
 
         for (addr in res.data.accounts) {
@@ -16,6 +17,7 @@ app.controller('indexCtrl', function($scope, $http) {
         }
     });
 
+    $http.get ('https://walletapi.kapu.one/api/delegates/get?username=billellera').then (function (res) {
         $scope.delegate = res.data.delegate;
     });
 });
